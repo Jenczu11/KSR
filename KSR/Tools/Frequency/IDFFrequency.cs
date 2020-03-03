5719d5464c8dedc5d0f05298c80c3f1e8a8382fd
PatrykZajac <patryk.zajac0@gmail.com>
2020-03-03 21:20:59 +0100

    Changes: Add FT&IDF frequency method

---
+++ KSR/Tools/Frequency/IDFFrequency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KSR.Tools.Frequency
+{
+    public class IDFFrequency : IFrequency
+    {
+
+        public Dictionary<string, decimal> Calc(List<string> words)
+        {
+            var count = words.Count;
+            var result = new Dictionary<string, decimal>();
+            var tfDict = new TFFrequency().Calc(words);
+            foreach (var item in tfDict)
+            {
+                result.Add(item.Key, item.Value / words.Count);
+            }
+            return result;
+        }
+    }
+}
---
+++ KSR/Tools/Frequency/TFFrequency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KSR.Tools.Frequency
+{
+    public class TFFrequency : IFrequency
+    {
+        public static Dictionary<string, decimal> Calc(List<string> words)
+        {
+            var result = new Dictionary<string, decimal>();
+            foreach (var word in words)
+            {
+                if (!result.ContainsKey(word))
+                {
+                    result.Add(word, 0);
+                }                result[word]++;
+            }
+            return result;
+        }
+    }
+}
