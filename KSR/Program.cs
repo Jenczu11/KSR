90da25470c8360acfee0f39de957c68b6c76ae4d
Jenczu11 <bartek.jencz@wp.pl>
2020-03-03 15:37:09 +0100

    Moved LINQ (?) to another class, filteredArticles are now another class

--- KSR/Program.cs
+++ KSR/Program.cs
@@ -3,28 +3,39 @@ using KSR.Tools.Readers;
 using System.Linq;
 using KSR.Model;
 using System.Collections.Generic;
+using KSR.Tools.Filter;
 
 namespace KSR
 {
     class Program
     {
         public static List<string> PLACES = new List<string>() { "west-germany", "usa", "france", "uk", "canada", "japan" };
+
         public const string PLACES_TAG = "places";
 
         public static void Main(string[] args)
         {
 
-            var reader = new ReutersReader();
-            var articles = reader.GetArticles();
-            var selected = articles
-                .Where(item => item.Tags.ContainsKey(PLACES_TAG))
-                .Where(item => item.Tags[PLACES_TAG].Count == 1)
-                .Where(item => PLACES.Contains(item.Tags[PLACES_TAG][0]))
-                .ToList();
-            Console.WriteLine("Po filtrowaniu"); 
-            selected[0].Paragraphs[0].ForEach(word => Console.WriteLine(word));
-            Console.ReadLine();
+            // var reader = new ReutersReader();
+            // var articles = reader.GetArticles();
+            // var selected = articles
+            //     .Where(item => item.Tags.ContainsKey(PLACES_TAG))
+            //     .Where(item => item.Tags[PLACES_TAG].Count == 1)
+            //     .Where(item => PLACES.Contains(item.Tags[PLACES_TAG][0]))
+            //     .ToList();
+            //
             // selected.ForEach(item => Console.WriteLine(string.Format("Title: {0}, DateLine: {1}, Place: {2}", item.Title, item.DateLine, item.Tags[PLACES_TAG][0])));
+            //
+           var reader = new ReutersReader();
+           var articles = reader.GetArticles();
+           
+           var filteredArticles = new FilteredArticles(articles);
+            // Console.WriteLine("Po filtrowaniu");
+            // Console.WriteLine(filteredArticles.GetArticleSingleParagraph(0, 1));
+            // string ParagraphText = string.Join(" ", filteredArticles.GetArticleSingleParagraph(0, 1).ToArray());
+            filteredArticles.printArticle(0);
+            // selected[0].Paragraphs[0].ForEach(word => Console.WriteLine(word));
+            Console.ReadLine();
         }
     }
 }
---
+++ KSR/Tools/Filter/FilteredArticles.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KSR.Model;
+
+namespace KSR.Tools.Filter
+{
+    
+    public class FilteredArticles
+    {
+        public static List<string> PLACES = new List<string>() { "west-germany", "usa", "france", "uk", "canada", "japan" };
+        public const string PLACES_TAG = "places";
+        public List<Article> selectedArticles;
+        public FilteredArticles(IEnumerable<Article> articles)
+        {
+            selectedArticles = articles
+                .Where(item => item.Tags.ContainsKey(PLACES_TAG))
+                .Where(item => item.Tags[PLACES_TAG].Count == 1)
+                .Where(item => PLACES.Contains(item.Tags[PLACES_TAG][0]))
+                .ToList();
+        }
+
+        public Article getArticle(int index)
+        {
+            return selectedArticles[index];
+        }
+
+        public List<List<string>> GetArticleManyParagraphs(int articleIndex)
+        {
+            return selectedArticles[articleIndex].Paragraphs;
+        }
+
+        public List<string> GetArticleSingleParagraph(int articleIndex, int paragraphIndex)
+        {
+            return selectedArticles[articleIndex].Paragraphs[paragraphIndex];
+        }
+
+        public int Count()
+        {
+            return selectedArticles.Count;
+        }
+
+        public void printArticle(int articleIndex)
+        {
+            selectedArticles[articleIndex].Paragraphs
+                .ForEach(paragraph => paragraph.ForEach(word =>
+                {
+                    Console.Write(word);
+                    Console.Write(" ");
+                }));
+
+        }
+        
+    }
+}
