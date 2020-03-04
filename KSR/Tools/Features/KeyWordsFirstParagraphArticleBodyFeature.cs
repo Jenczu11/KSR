d72f5abcb199e9baa26e7213ff09ea67de49c490
PatrykZajac <patryk.zajac0@gmail.com>
2020-03-04 12:36:06 +0100

    Changes: Add feature keywords in first paragraph

---
+++ KSR/Tools/Features/KeyWordsFirstParagraphArticleBodyFeature.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using KSR.Model;
+
+namespace KSR.Tools.Features
+{
+    public class KeyWordsFirstParagraphArticleBodyFeature : IFeature
+    {
+        public decimal Calc(Article article, List<string> keyWords)
+        {
+            var count = 0;
+            var words = article.Paragraphs[0];
+            foreach (var item in keyWords)
+            {
+                if (words.Contains(item))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
