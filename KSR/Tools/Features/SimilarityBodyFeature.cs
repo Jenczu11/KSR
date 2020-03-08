ef2a415d8dc9522f26e47d5c130300dc9e220cec
PatrykZajac <patryk.zajac0@gmail.com>
2020-03-08 19:56:13 +0100

    Changes: Add similarity feature

---
+++ KSR/Tools/Features/SimilarityBodyFeature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using KSR.Model;
+using KSR.Tools.SimliarityFunctions;
+
+namespace KSR.Tools.Features
+{
+    public class SimilarityBodyFeature : IFeature
+    {
+        public decimal Calc(Article article, List<string> keyWords, ISimilarityFunction similarityFunction)
+        {
+            var count = 0m;
+            var words = article.GetAllWords();
+            foreach (var word in keyWords)
+            {
+                words.ForEach(item => count += similarityFunction.CalculateSimilarity(item, word));
+            }
+            return count;
+        }
+    }
+}
