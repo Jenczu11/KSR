16be6e1c7d4dc8f7566fcf01e8ce1a86e8a16a9a
Jenczu11 <bartek.jencz@wp.pl>
2020-03-28 21:31:05 +0100

    Implemented CustomMetric1

---
+++ KSR/Tools/Metrics/CustomMetric1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json.Schema;
+using NumSharp;
+
+namespace KSR.Tools.Metrics
+{
+    public class CustomMetric1 : IMetric
+    {
+        public double GetDistance(NDArray lhs, NDArray rhs)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double GetDistance(double[] lhs, double[] rhs)
+        {
+            var result = 0d;
+            for (var i = 0; i < lhs.Length; i++)
+            {
+                
+                result += Math.Pow(lhs[i] + rhs[i], 2);
+            }
+            return Math.Sqrt(result);
+        }
+
+        public override string ToString()
+        {
+            return "CustomMetric1 -> sqrt(sigma(a+b)^2)";
+        }
+    }
+}
