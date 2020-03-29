63badeac2587635c5abbc69a1cb97638b34fa5c4
Jenczu11 <bartek.jencz@wp.pl>
2020-03-29 13:13:37 +0200

    Implemented CanberraMeric

---
+++ KSR/Tools/Metrics/CanberraMetric.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using NumSharp;
+
+namespace KSR.Tools.Metrics
+{
+    public class CanBerraMetric : IMetric
+    {
+        public double GetDistance(NDArray lhs, NDArray rhs)
+        {
+            return np.sum(np.divide(
+                np.abs(np.subtract(lhs, rhs)),
+                np.add(np.abs(lhs), np.abs(rhs))
+            ));
+        }
+
+        public double GetDistance(double[] lhs, double[] rhs)
+        {
+            var result = 0d;
+            for (var i = 0; i < lhs.Length; i++)
+            {
+                result += Math.Abs(lhs[i] - rhs[i]) / (Math.Abs(lhs[i]) + Math.Abs(rhs[i]));
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return "CanberraMetric";
+        }
+    }
+}
